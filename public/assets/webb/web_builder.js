15af1fe8af780c1e231376cbc1406ef0e99b5ec5
sienekib20 <sienekib@gmail.com>
2023-11-29 14:21:59 +0100

    updates

--- public/assets/webb/web_builder.js
+++ public/assets/webb/web_builder.js
@@ -1,35 +1,6 @@
-// Open close toolbars
-const openclose = document.querySelector('.openclose');
-const web_builder = document.querySelector('.web_builder');
-const web_builder_tools = document.querySelector('.web_builder_tools');
-openclose.addEventListener('click', (e) => {
-	//e.preventDefault();
-	if (web_builder.classList.length == 1) {
-		web_builder.classList.add('active');
-		web_builder_tools.classList.add('active');
-		web_builder.style.width = '100%';
-		e.target.style.right='0%';
-		web_builder_tools.style.width = '0%';
-	} else {
-		
-		web_builder.classList.remove('active');
-		web_builder_tools.classList.remove('active');
-		web_builder.style.width = '80%';
-		web_builder_tools.style.width = '20%';
-		e.target.style.right='18.5%';
-	}
-});
-
-// Tabs change
-const tabs_items = document.querySelectorAll('.tools_header_item');
-const webb_contents = document.querySelectorAll('.builder_tools_contain > div');
-
-tabs_items.forEach((item, key) => {
-	item.addEventListener('click', (e) => {
-		e.preventDefault();
-		tabs_items.forEach((item) => item.classList.remove('active'));
-		webb_contents.forEach((item) => item.classList.remove('active'));
-		item.classList.add('active');
-		webb_contents[key].classList.add('active');
-	});
+const editor = grapesjs.init({
+	container: '.web_builder',
+	fromElement: true,
+	storageManager: false,
+	panels: { defaults: [] },
 });
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ $routes->addRoute('/user/[0-9]+', [AppController::class, 'update']);
 $routes->addRoute('/templates', [Templates::class, 'load_templates']);
 $routes->addRoute('/api/template', [Templates::class, 'upload_template']);
 
-//$routes->addRoute('/editor/[0-9]+', [Templates::class, 'open_editor']);
+$routes->addRoute('/editor/[0-9]+', [Templates::class, 'open_editor']);
 
-$routes->addRoute('/editor/[0-9]+', [Editor::class, 'open_template']);
+//$routes->addRoute('/editor/[0-9]+', [Editor::class, 'open_template']);
 
 $routes->addRoute('/web_builder', [Editor::class, 'web_builder']);
 
