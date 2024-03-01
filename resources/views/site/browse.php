36c9fd592c4a7bf7bce06b304828af1638821fab
sienekib20 <sienekib@gmail.com>
2024-03-01 14:43:29 +0100

    updates

--- resources/views/site/browse.php
+++ resources/views/site/browse.php
@@ -20,46 +20,9 @@
 <body>
 
     <div class="wrapper">
-        <?= parts('nav.header-search') ?>
+        <?= parts('nav.wr-navbar') ?>
 
-        <header class="wr-header vh-55">
-            <div class="wr-overlay"></div>
-            <img src="<?= asset('img/pexels-jopwell-2422293.jpg') ?>" alt="">
-            <div class="d-flex mt-5"></div>
-            <div class="wr-header-text">
-                <div class="container-sm">
-                    <div class="row">
-                        <form action="" class="col-12 mx-auto col-md-10 col-lg-7" id="searchTemplateInList">
-                            <div class="card-title text-center">
-                                <h4 class="title d-block text-white">Filtre a sua busca</h4>
-                            </div>
-                            <div class="input-group">
-                                <input type="text" class="form-input" placeholder="Pesquise por template, autor...">
-                                <small class="bi bi-search"></small>
-                            </div>
-                            <div class="row row-no-margin mt-4">
-                                <select id="" class="col-md-6 form-input">
-                                    <option value="">Categorias</option>
-                                    <?php foreach ($categorias as $categoria) : ?>
-                                        <option value="<?= $categoria->categoria_id ?>"><?= $categoria->categoria ?></option>
-                                    <?php endforeach; ?>
-                                </select>
-                                <select id="" class="mt-3 mt-md-0 col-md-6 form-input">
-                                    <option value="">Tipo template</option>
-                                    <?php foreach ($tipo as $t) : ?>
-                                        <option value="<?= $t->tipo_template_id ?>"><?= $t->tipo_template ?></option>
-                                    <?php endforeach; ?>
-                                </select>«
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-        </header>
+        <small class="d-flex my-5"></small>
 
         <div class="card">
             <div class="container-sm">
@@ -74,10 +37,37 @@
             </div>
         </div>
 
+        <div class="card-top">
+            <div class="container-sm">
+                <form action="" class="row" id="searchTemplateInList">
+                    <div class="input-group col-md-6">
+                        <input type="text" class="form-input" placeholder="Pesquise por template, autor...">
+                        <small class="bi bi-search d-flex mr-3"></small>
+                    </div>
+                    <div class="input-group col-md-3">
+                        <select id="" class="form-select">
+                            <option value="">Categorias</option>
+                            <?php foreach ($categorias as $categoria) : ?>
+                                <option value="<?= $categoria->categoria_id ?>"><?= $categoria->categoria ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="input-group col-md-3">
+                        <select id="" class="form-select">
+                            <option value="">Tipo template</option>
+                            <?php foreach ($tipo as $t) : ?>
+                                <option value="<?= $t->tipo_template_id ?>"><?= $t->tipo_template ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        </div>
+
         <div class="card">
             <div class="container-sm">
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-12 px-0">
                         <div class="row">
                             <?php foreach ($templates as $template) : ?>
                                 <div class="col-12 col-md-5 col-lg-3">
@@ -109,3 +99,7 @@
 </body>
 
 </html>
+
+<script>
+    applyDarkNavbar();
+</script>
